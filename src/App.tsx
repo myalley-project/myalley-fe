121417840fbc25ec4717ac06f30f8c90da0e76d3
YesunPark <lynn08082@gmail.com>
2022-12-20 11:42:35 +0900

    Setting: 기본 컴포넌트 파일 생성, 라우팅 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Main from "./pages/main/Main";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signUp/SignUp";
+import ExhibitionList from "./pages/exhibitionList/ExhibitionList";
+import Exhibition from "./pages/exhibition/Exhibition";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div />;
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup/:type" element={<SignUp />} />
+        <Route path="/exhibition-list" element={<ExhibitionList />} />
+        <Route path="/exhibition/:id" element={<Exhibition />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/exhibitionList/ExhibitionList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ExhibitionList() {
+  return <div />;
+}
+
+export default ExhibitionList;
