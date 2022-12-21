c302f976c159a92805946393fd163819ebe01e58
YesunPark <lynn08082@gmail.com>
2022-12-22 00:36:57 +0900

    Feat: 회원용 회원가입페이지 레이아웃 작업 중 #4
    - 로그인/회원가입 공통 스타일컴포넌트를 활용해 레이아웃 구성
    - 회원가입에만 있는 input입력창 추가
    - url에 따라 회원/관리자로 버전이 다르게 보여지도록 구현
    - 가장 기본적인 UI만 구현, 디테일은 추가예정

--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -1,11 +1,65 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
 import LoginSignUp from "../../components/LoginSignUp.style";
+import { dayArr, monthArr, yearArr } from "../../function/signUp/signUpBirth";
 
-const SignUp = () => (
-  <LoginSignUp title="회원가입">
-    <input />
-    <button type="submit">회원가입하기</button>
-  </LoginSignUp>
-);
+const SignUp = () => {
+  const location = useLocation();
+
+  return (
+    <LoginSignUp title="회원가입">
+      <input name="email" placeholder="ex_mail@exhibition.com" />
+      <input name="pw" placeholder="Password" />
+      <input name="pwValid" placeholder="Password" />
+      {location.search !== "?admin" && (
+        <>
+          <span>별명</span>
+          <input type="text" placeholder="Nickname" />
+          <span>생년월일</span>
+          <BirthDropDownContainer>
+            <select>
+              <option>년도</option>
+              {yearArr().map((year) => (
+                <option key={year}>{year}</option>
+              ))}
+            </select>
+            <select>
+              <option>월</option>
+              {monthArr().map((month) => (
+                <option key={month}>{month}</option>
+              ))}
+            </select>
+            <select>
+              <option>일</option>
+              {dayArr().map((day) => (
+                <option key={day}>{day}</option>
+              ))}
+            </select>
+          </BirthDropDownContainer>
+          <span>성별</span>
+          <GenderDropDown>
+            <option>선택없음</option>
+            <option>남</option>
+            <option>여</option>
+          </GenderDropDown>
+        </>
+      )}
+      {location.search === "?admin" && (
+        <>
+          <span>관리자 고유번호</span>
+          <input />
+          <span>이름</span>
+          <input />
+        </>
+      )}
+      <button type="submit"> 가입하기</button>
+    </LoginSignUp>
+  );
+};
+
+const BirthDropDownContainer = styled.div``;
+
+const GenderDropDown = styled.select``;
 
 export default SignUp;
