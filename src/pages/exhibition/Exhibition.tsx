7b51d1338e8d5744e03eff854f1fa7efb06f227b
gandy818 <gandy818@gmail.com>
2022-12-22 18:23:14 +0900

    Feat: 전시회 상세페이지 뼈대 구축

--- src/pages/exhibition/Exhibition.tsx
+++ src/pages/exhibition/Exhibition.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Exhibition() {
-  return <div />;
+  return (
+    <div>
+      <div>header</div>
+      <div>
+        <div>상세정보/전시리뷰 버튼</div>
+        <div>기획의도 box</div>
+        <div>전시내용 box</div>
+        <div>작가 정보 box</div>
+      </div>
+    </div>
+  );
 }
 
 export default Exhibition;
