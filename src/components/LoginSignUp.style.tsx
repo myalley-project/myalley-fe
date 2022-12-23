76103158b202fc4312abb86746cac2c3c7e017d9
YesunPark <lynn08082@gmail.com>
2022-12-23 21:50:58 +0900

    Design: 로그인/회원가입 공통 컴포넌트 수정 #4
    - input padding, 배경, border-color 변경
    - width 수정

--- src/components/LoginSignUp.style.tsx
+++ src/components/LoginSignUp.style.tsx
@@ -37,11 +37,8 @@ const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 19.79vw;
-  min-width: 200px;
-  //반응형으로 하려고 vw를 사용하다보니 화면이 줄었을 때 좀 부자연스러워서
-  //디자이너분께 최소 넓이 여쭤봐야 함.
-  border: 1px solid black;
+  width: 380px;
+  border: 1px solid #e0e0e0;
   border-radius: 30px;
   //border-radius는 거의 30픽셀이라서 global-style에 추가해서 사용하면 좋을 것 같습니다.
   .title {
@@ -51,16 +48,21 @@ const InputContainer = styled.div`
   input {
     width: 84.21%;
     height: 40px;
-    margin-bottom: 10px;
-    border: 1px solid black;
+    padding-left: 20px;
+    border: 1px solid #e0e0e0;
     border-radius: 30px;
+    background-color: #fbfbfb;
   }
   button {
     width: 84.21%;
     height: 48px;
     margin: 30px;
-    border: 1px solid black;
+    border: none;
     border-radius: 30px;
+    background-color: #9c9c9c;
+    color: white;
+    font-size: 18px;
+    font-weight: 700;
   }
 `;
 
