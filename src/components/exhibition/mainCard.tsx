600b51d4ad7de718eaf45d3cbf2778140bb47c42
gandy818 <gandy818@gmail.com>
2022-12-25 18:55:44 +0900

    Design : 전시상세페이지 메인 카드 컴포넌트 추가 #14

---
+++ src/components/exhibition/mainCard.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import styled from "styled-components";
+
+// 나중에 수정 필요
+interface MainCardType {
+  title: string;
+  date: string;
+  place: string;
+  time: string;
+  charge: string;
+}
+
+function MainCard({ title, date, place, time, charge }: MainCardType) {
+  return (
+    <Header>
+      <Card>
+        <ImageContainer></ImageContainer>
+        <InfoContainer>
+          <Title>{title}</Title>
+          <div style={{ padding: "30px 0" }}>
+            <InfoDetail>
+              <dt>시간</dt>
+              <dd>{date}</dd>
+            </InfoDetail>
+            <InfoDetail>
+              <dt>장소</dt>
+              <dd>{place}</dd>
+            </InfoDetail>
+            <InfoDetail>
+              <dt>시간</dt>
+              <dd>{time}</dd>
+            </InfoDetail>
+            <InfoDetail style={{ marginBottom: "0px" }}>
+              <dt>관람비용</dt>
+              <dd>{charge}</dd>
+            </InfoDetail>
+          </div>
+          <Footer>
+            <div>사이트 방문</div>
+            {/* 추후 아이콘 추가예정 */}
+            <div></div>
+            <div></div>
+          </Footer>
+        </InfoContainer>
+      </Card>
+    </Header>
+  );
+}
+
+export default MainCard;
+
+const Header = styled.div`
+  display: flex;
+  width: 100%;
+  height: 458px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.theme.colors.main};
+`;
+
+const Card = styled.div`
+  display: flex;
+  width: 1200px;
+  height: 358px;
+  border: 1px solid #e0e0e0;
+  background-color: #ffffff;
+`;
+
+const ImageContainer = styled.div`
+  width: 278px;
+  height: 100%;
+  background-color: #d9d9d9;
+`;
+
+const InfoContainer = styled.div`
+  width: 922px;
+  height: 358px;
+  padding: 30px;
+`;
+
+const Title = styled.h1`
+  width: 100%;
+  height: 104px;
+  font-weight: 700;
+  font-size: 42px;
+  line-height: 52px;
+  letter-spacing: -0.5px;
+  color: #333333;
+  text-align: left;
+`;
+
+const InfoDetail = styled.dl`
+  display: flex;
+  width: fit-content;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: -0.5px;
+  color: #666666;
+  text-align: left;
+  margin-bottom: 10px;
+  dt {
+    width: 72px;
+    font-weight: 500;
+    margin-right: 30px;
+  }
+  dd {
+    font-weight: 400;
+  }
+`;
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  font-weight: 500;
+  font-size: 14px;
+  letter-spacing: -0.5px;
+  color: #9c9c9c;
+  text-align: right;
+`;
--- src/pages/exhibition/Exhibition.tsx
+++ src/pages/exhibition/Exhibition.tsx
@@ -1,17 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import MainCard from "../../components/exhibition/mainCard";
 
 function Exhibition() {
   return (
-    <div>
-      <div>header</div>
-      <div>
+    <ExhibitionContainer>
+      <MainCard
+        title="DDP 오픈큐레이팅 vol.26 <Beyond The City: Cultural Monuments>"
+        date="2022-12-14 ~ 2023-01-15"
+        place="DDP"
+        time="10:00 ~ 20:00"
+        charge="무료"
+      ></MainCard>
+
+      {/* <div>
         <div>상세정보/전시리뷰 버튼</div>
         <div>기획의도 box</div>
         <div>전시내용 box</div>
         <div>작가 정보 box</div>
-      </div>
-    </div>
+      </div> */}
+    </ExhibitionContainer>
   );
 }
 
 export default Exhibition;
+
+const ExhibitionContainer = styled.div`
+  width: 1920px;
+  text-align: center;
+`;
