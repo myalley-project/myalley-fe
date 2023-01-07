188a47cb712ea502640e3a9a8fd2d21748bc7e69
gandy818 <gandy818@gmail.com>
2023-01-08 02:07:58 +0900

    design: 마우스오버시 메뉴 아이콘 변화 동작 추가

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import UserMenu from "../assets/icons/UserMenu.svg";
+import UserMenuHover from "../assets/icons/userMenuHover.svg";
+import UserMenu from "../assets/icons/userMenu.svg";
 import HamburgerMenu from "./HamburgerMenu";
 
 const Nav = () => {
-  const [showMenu, setShowMenu] = useState(false);
-  const toggleMenu = () => {
-    setShowMenu((prevState) => !prevState);
+  const [isShowMenu, setIsShowMenu] = useState(false);
+  const userMenuHover = UserMenuHover;
+  const userMenu = UserMenu;
+  const menuImg = document.getElementById(
+    "user-menu"
+  ) as HTMLInputElement | null;
+
+  const handleToggleMenu = () => {
+    setIsShowMenu((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    if (menuImg !== null) {
+      if (isShowMenu) {
+        menuImg.setAttribute("src", userMenuHover);
+      } else {
+        menuImg.setAttribute("src", userMenu);
+      }
+    }
+  }, [isShowMenu, menuImg, userMenu, userMenuHover]);
+
+  const changeToHoverIcon = () => {
+    if (menuImg !== null && !isShowMenu)
+      menuImg.setAttribute("src", userMenuHover);
+  };
+
+  const changeToNormalIcon = () => {
+    if (menuImg !== null && !isShowMenu) menuImg.setAttribute("src", userMenu);
   };
 
   return (
     <Navbar>
       <Logo />
-      <button type="button" onClick={toggleMenu}>
-        <img src={UserMenu} alt="menu-icon" />
-      </button>
-      {showMenu && <HamburgerMenu />}
+      <MenuButton
+        type="button"
+        onClick={handleToggleMenu}
+        onMouseOver={changeToHoverIcon}
+        onMouseLeave={changeToNormalIcon}
+      >
+        <img src={UserMenu} alt="menu-icon" id="user-menu" />
+      </MenuButton>
+      {isShowMenu && <HamburgerMenu />}
     </Navbar>
   );
 };
@@ -25,18 +56,23 @@ export default Nav;
 const Navbar = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   width: 75vw;
   height: 70px;
   margin: 0 auto;
   border-radius: 0px;
   background-color: #ffffff;
-  > button {
-    cursor: pointer;
-  }
 `;
 
 const Logo = styled.div`
   width: 210px;
+  height: 100%;
   border-radius: 0px;
   background-color: ${(props) => props.theme.colors.main};
+`;
+
+const MenuButton = styled.button`
+  width: 36px;
+  height: 36px;
+  cursor: pointer;
 `;
