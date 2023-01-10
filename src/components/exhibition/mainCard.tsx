7d03db402c7e0ea8b1f1c2c6c791f22294bb7a4c
Nayoung Yu <67881881+gandy818@users.noreply.github.com>
2023-01-10 21:35:07 +0900

    !HOTFIX: 파일명, 오탈자 수정하여 에러 해결 (#44)
    * fix git hooks
    
    .husky pre-commit 파일 수정
    package.json script 수정
    
    -----------------------------------------------
    [체크리스트]
    
        제목 첫 글자는 대문자로 작성했나요?
        제목은 명령문으로 작성했나요?
        제목 끝에 마침표 금지
        제목과 본문을 한 줄 띄워 분리하기
        본문에 여러 줄의 메세지를 작성할 때는 "~"로 구분하기
    -----------------------------------------------
    
    * !HOTFIX: 파일명 수정하여 대소문자 에러 해결
    
    * Merge: main 병합
    
    * Delete: 소문자 파일 삭제
    
    * !Hotfix: styled-component 오탈자 수정, 병합 안된 부분 수동으로 처리

--- src/components/exhibition/mainCard.tsx
+++ src/components/exhibition/mainCard.tsx
@@ -1,51 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import Like from "../../assets/icons/like.svg";
-import Share from "../../assets/icons/share.svg";
+import likeIcon from "../../assets/icons/like.svg";
+import shareIcon from "../../assets/icons/share.svg";
 
-// 나중에 수정 필요
-interface MainCardType {
-  title: string;
-  date: string;
-  place: string;
-  time: string;
-  charge: string;
-}
+type MainCardType = {
+  [key in "title" | "date" | "place" | "time" | "charge"]: string;
+};
 
-const MainCard = ({ title, date, place, time, charge }: MainCardType) => (
-  <CardContainer>
-    <Card>
-      <ImageContainer />
-      <InfoContainer>
-        <Title>{title}</Title>
-        <div style={{ padding: "30px 0" }}>
-          <InfoDetail>
-            <dt>시간</dt>
-            <dd>{date}</dd>
-          </InfoDetail>
-          <InfoDetail>
-            <dt>장소</dt>
-            <dd>{place}</dd>
-          </InfoDetail>
-          <InfoDetail>
-            <dt>시간</dt>
-            <dd>{time}</dd>
-          </InfoDetail>
-          <InfoDetail style={{ marginBottom: "0px" }}>
-            <dt>관람비용</dt>
-            <dd>{charge}</dd>
-          </InfoDetail>
-        </div>
-        <Footer>
-          {/* 나중에 링크로 수정 */}
-          <p>사이트 방문</p>
-          <img src={Like} alt="like icon" />
-          <img src={Share} alt="share icon" />
-        </Footer>
-      </InfoContainer>
-    </Card>
-  </CardContainer>
-);
+const MainCard = ({ title, date, place, time, charge }: MainCardType) => {
+  const [cardHeight, setCardHeight] = useState("358px");
+  const [auth, setAuth] = useState("user");
+
+  useEffect(() => {
+    if (auth === "admin") {
+      setCardHeight("412px");
+    } else setCardHeight("358px");
+  }, [auth, cardHeight]);
+
+  return (
+    <CardContainer>
+      <Card height={cardHeight}>
+        <ImageContainer />
+        <InfoContainer height={cardHeight}>
+          {auth === "admin" && (
+            <EditButtons>
+              <Button>수정</Button>
+              <Button>삭제</Button>
+            </EditButtons>
+          )}
+          <Title>{title}</Title>
+          <div style={{ padding: "30px 0" }}>
+            <InfoDetail>
+              <dt>시간</dt>
+              <dd>{date}</dd>
+            </InfoDetail>
+            <InfoDetail>
+              <dt>장소</dt>
+              <dd>{place}</dd>
+            </InfoDetail>
+            <InfoDetail>
+              <dt>시간</dt>
+              <dd>{time}</dd>
+            </InfoDetail>
+            <InfoDetail style={{ marginBottom: "0px" }}>
+              <dt>관람비용</dt>
+              <dd>{charge}</dd>
+            </InfoDetail>
+          </div>
+          <Footer>
+            {/* 나중에 링크로 수정 */}
+            <p>사이트 방문</p>
+            <img src={likeIcon} alt="like icon" />
+            <img src={shareIcon} alt="share icon" />
+          </Footer>
+        </InfoContainer>
+      </Card>
+    </CardContainer>
+  );
+};
 
 export default MainCard;
 
@@ -59,11 +71,11 @@ const CardContainer = styled.div`
   background-color: #f5f5f5;
 `;
 
-const Card = styled.div`
+const Card = styled.div<{ height: string }>`
   display: flex;
   max-width: 1200px;
   width: 83vw;
-  height: 358px;
+  height: ${(props) => props.height};
   border: 1px solid #e0e0e0;
   background-color: #ffffff;
 `;
@@ -74,10 +86,29 @@ const ImageContainer = styled.div`
   background-color: #d9d9d9;
 `;
 
-const InfoContainer = styled.div`
+const InfoContainer = styled.div<{ height: string }>`
   width: 922px;
-  height: 358px;
+  height: ${(props) => props.height};
   padding: 30px;
+`;
+
+const EditButtons = styled.div`
+  display: flex;
+  gap: 20px;
+  justify-content: flex-end;
+  height: 30px;
+  margin-bottom: 30px;
+`;
+
+const Button = styled.button`
+  padding: 0;
+  color: ${(props) => props.theme.colors.hover};
+  font-size: 14px;
+  cursor: pointer;
+  &:hover {
+    font-weight: 700;
+    color: ${(props) => props.theme.colors.txt};
+  }
 `;
 
 const Title = styled.h1`
@@ -87,7 +118,7 @@ const Title = styled.h1`
   font-size: 42px;
   line-height: 52px;
   letter-spacing: -0.5px;
-  color: ${(props) => props.theme.colors.txt}
+  color: ${(props) => props.theme.colors.txt};
   text-align: left;
 `;
 
@@ -98,7 +129,7 @@ const InfoDetail = styled.dl`
   font-size: 14px;
   line-height: 20px;
   letter-spacing: -0.5px;
-  color: ${(props) => props.theme.colors.pressed}
+  color: ${(props) => props.theme.colors.pressed};
   text-align: left;
   margin-bottom: 10px;
   dt {
@@ -119,7 +150,7 @@ const Footer = styled.div`
   font-weight: 500;
   font-size: 14px;
   letter-spacing: -0.5px;
-  color: ${(props) => props.theme.colors.hover}
+  color: ${(props) => props.theme.colors.hover};
   text-align: right;
   p {
     line-height: 24px;
