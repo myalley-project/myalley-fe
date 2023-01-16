5f72f7af02f9ca0297e06aea84e9e3bc57680f2c
gandy818 <gandy818@gmail.com>
2023-01-17 00:44:09 +0900

    Feat: 전시 상세페이지 기능 추가

--- src/pages/Exhibition.tsx
+++ src/pages/Exhibition.tsx
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import MainCard from "../components/exhibition/MainCard";
 import ContentCard from "../components/exhibition/ContentCard";
 import ToggleSwitch from "../components/exhibition/ToggleSwitch";
 
-const Exhibition = () => (
-  <ExhibitionContainer>
-    <MainCard
-      title="DDP 오픈큐레이팅 vol.26 <Beyond The City: Cultural Monuments>"
-      date="2022-12-14 ~ 2023-01-15"
-      place="DDP"
-      charge="무료"
-    />
-    <ToggleSwitch />
-    <ContentCard title="전시 내용" content="lorem" />
-    <ContentCard title="작가 정보" content="lorem" />
-  </ExhibitionContainer>
-);
+const Exhibition = () => {
+  const [state, setState] = useState("info");
+  return (
+    <ExhibitionContainer>
+      <MainCard
+        title="DDP 오픈큐레이팅 vol.26 <Beyond The City: Cultural Monuments>"
+        date="2022-12-14 ~ 2023-01-15"
+        place="DDP"
+        charge="무료"
+      />
+      <ToggleSwitch setState={setState} />
+      {state === "info" && (
+        <>
+          <ContentCard title="전시 내용" content="lorem" />
+          <ContentCard title="작가 정보" content="lorem" />
+        </>
+      )}
+    </ExhibitionContainer>
+  );
+};
 
 export default Exhibition;
 
