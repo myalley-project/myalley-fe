b530157410a32bd4324c7c4b48424c24b948eaef
caffesale <pglibrary80@gmail.com>
2023-01-17 22:50:26 +0900

    Feat: 전시회 모달 선택버튼 작성 #66

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import Editor from "../components/Editor";
 import TimeSelect from "../components/blogreview/TimeSelect";
 import ConvenienceSelect from "../components/blogreview/ConvenienceSelect";
+import ExhibitionSelect from "../components/blogreview/ExhibitionSelect";
 
 const Main = () => (
   <>
+    <ExhibitionSelect />
     <TimeSelect />
     <ConvenienceSelect />
   </>
