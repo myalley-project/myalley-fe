3aeb98af5c1c2b5a40efd43e50333f7714341830
caffesale <pglibrary80@gmail.com>
2023-01-17 23:33:50 +0900

    Feat: 블로그 리뷰 컴포넌트 모두 완성 #66
    TittleInput.tsx 작성

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,9 +3,11 @@ import Editor from "../components/Editor";
 import TimeSelect from "../components/blogreview/TimeSelect";
 import ConvenienceSelect from "../components/blogreview/ConvenienceSelect";
 import ExhibitionSelect from "../components/blogreview/ExhibitionSelect";
+import ReviewTitle from "../components/blogreview/ReviewTitle";
 
 const Main = () => (
   <>
+    <ReviewTitle />
     <ExhibitionSelect />
     <TimeSelect />
     <ConvenienceSelect />
