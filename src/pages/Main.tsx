0fc95d0c77adeabd7b013c422ac3ab02abe22b3d
caffesale <pglibrary80@gmail.com>
2023-01-18 23:55:35 +0900

    Docs: 테스트용 메인 페이지 컴포넌트 정상화

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import Editor from "../components/Editor";
-import TimeSelect from "../components/blogreview/TimeSelect";
-import ConvenienceSelect from "../components/blogreview/ConvenienceSelect";
-import ExhibitionSelect from "../components/blogreview/ExhibitionSelect";
-import ReviewTitle from "../components/blogreview/ReviewTitle";
 
-const Main = () => <Editor />;
+const Main = () => <div />;
 
 export default Main;
