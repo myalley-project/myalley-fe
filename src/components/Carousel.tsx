f01920b5eb0a2684a259740676fd3c13f04d98e6
caffesale <pglibrary80@gmail.com>
2023-01-18 12:39:59 +0900

    Feat: 슬라이드 이미지와 블러 효과 추가 #60
    메인 페이지 캐러셀 이미지 적용
    메인 페이지 캐러셀 블러 효과 적용

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,46 +6,85 @@ import "swiper/css";
 import "swiper/css/scrollbar";
 import "swiper/css/navigation";
 
-const Carousel = () => (
-  <StyledSwiper
-    scrollbar
-    navigation
-    modules={[Navigation, Scrollbar]}
-    className="mySwiper"
-  >
-    <SwiperSlide>
-      <Slide>
-        <div>최근 뜨고 있는 전시</div>
-        <div>
-          Beyond The City:
-          <br /> Cultural Monuments
-        </div>
-      </Slide>
-    </SwiperSlide>
-    <SwiperSlide>
-      <Slide>
-        <div>최근 뜨고 있는 전시</div>
-        <div>
-          Beyond The City:
-          <br />
-          Cultural Monuments
-        </div>
-      </Slide>
-    </SwiperSlide>
-    <SwiperSlide>
-      <Slide>
-        <div>최근 뜨고 있는 전시</div>
-        <div>
-          Beyond The City:
-          <br />
-          Cultural Monuments
-        </div>
-      </Slide>
-    </SwiperSlide>
-  </StyledSwiper>
-);
+import firstSlideImage from "../assets/images/firstSlideImage.jpg";
+import secondSlideImage from "../assets/images/secondSlideImage.jpg";
+import thirdSlideImage from "../assets/images/thirdSlideImage.jpg";
+
+const Carousel = () => {
+  const firstSlideStyle = {
+    backgroundImage: `url(${firstSlideImage})`,
+    backgroundRepeat: `no-repeat`,
+    backgroundPosition: `top center`,
+    backgroundSize: `cover`,
+    backgroundAttachment: `fixed`,
+    // filter: `blur(5px)`,
+    // webkitFilter: `blur(5px)`,
+  };
+
+  const secondSlideStyle = {
+    backgroundImage: `url(${secondSlideImage})`,
+    backgroundRepeat: `no-repeat`,
+    backgroundPosition: `top center`,
+    backgroundSize: `cover`,
+    backgroundAttachment: `fixed`,
+  };
+
+  const thirdSlideStyle = {
+    backgroundImage: `url(${thirdSlideImage})`,
+    backgroundRepeat: `no-repeat`,
+    backgroundPosition: `top center`,
+    backgroundSize: `cover`,
+    backgroundAttachment: `fixed`,
+  };
+
+  return (
+    <Container>
+      <StyledSwiper
+        scrollbar
+        navigation
+        modules={[Navigation, Scrollbar]}
+        className="mySwiper"
+      >
+        <SwiperSlide style={firstSlideStyle}>
+          <Slide />
+          <Text>
+            <div>최근 뜨고 있는 전시</div>
+            <div>
+              Beyond The City:
+              <br /> Cultural Monuments
+            </div>
+          </Text>
+        </SwiperSlide>
+        <SwiperSlide style={secondSlideStyle}>
+          <Slide />
+          <Text>
+            <div>최근 뜨고 있는 전시</div>
+            <div>
+              Beyond The City:
+              <br /> Cultural Monuments
+            </div>
+          </Text>
+        </SwiperSlide>
+        <SwiperSlide style={thirdSlideStyle}>
+          <Slide />
+          <Text>
+            <div>최근 뜨고 있는 전시</div>
+            <div>
+              Beyond The City:
+              <br /> Cultural Monuments
+            </div>
+          </Text>
+        </SwiperSlide>
+      </StyledSwiper>
+    </Container>
+  );
+};
 
 export default Carousel;
+
+const Container = styled.div`
+  display: flex;
+`;
 
 const StyledSwiper = styled(Swiper)`
   .swiper-wrapper {
@@ -53,9 +92,7 @@ const StyledSwiper = styled(Swiper)`
     height: 678px;
   }
   & .swiper-slide {
-    display: grid;
-    justify-content: center;
-    align-items: center;
+    position: relative;
   }
   & .swiper-scrollbar {
     display: block !important;
@@ -69,13 +106,26 @@ const StyledSwiper = styled(Swiper)`
   & .swiper-scrollbar-drag {
     background-color: #fff;
   }
-  & .swiper-button-prev,
-  & .swiper-button-next {
+  & > .swiper-button-prev,
+  & > .swiper-button-next {
     color: #9c9c9c;
   }
 `;
 
 const Slide = styled.div`
+  position: relative;
+  height: 100%;
+  width: 100%;
+  backdrop-filter: blur(5px);
+`;
+
+const Text = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  text-align: center;
   & > div:first-child {
     color: #9c9c9c;
     font-size: 28px;
