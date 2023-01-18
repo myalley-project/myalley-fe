e16a6c1a93979a8587e88022172d42b20783ec7f
caffesale <pglibrary80@gmail.com>
2023-01-18 21:16:24 +0900

    Design: 캘린더 컴포넌트 타이틀 삭제 #66
    컨테이너 제거
    타이틀 삭제
    달력 컴포넌트만 잔재함

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -58,47 +58,40 @@ const Calender = () => {
   };
 
   return (
-    <Container>
-      <Title>관람일</Title>
-      <CalendarWrapper>
-        <Header>
-          <div>
-            <button type="button" onClick={prevMonth}>
-              <img src={ArrowPrev} alt="이전 화살표" />
-            </button>
-          </div>
-          <div>
-            {format(currentMonth, "M")}월 {format(currentMonth, "yyyy")}
-          </div>
-          <div>
-            <button type="button" onClick={nextMonth}>
-              <img src={ArrowNext} alt="다음 화살표" />
-            </button>
-          </div>
-        </Header>
-        <Divider />
-        <DayOfWeek>
-          <div>일</div>
-          <div>월</div>
-          <div>화</div>
-          <div>수</div>
-          <div>목</div>
-          <div>금</div>
-          <div>토</div>
-        </DayOfWeek>
-        <Week
-          onClick={onDateSelect}
-          onKeyDown={onDateSelect}
-          role="presentation"
-        >
-          {calendarDays.map((each) => (
-            <Day key={each.id} data-valid={each.isValid} data-value={each.day}>
-              {each.day}
-            </Day>
-          ))}
-        </Week>
-      </CalendarWrapper>
-    </Container>
+    <CalendarWrapper>
+      <Header>
+        <div>
+          <button type="button" onClick={prevMonth}>
+            <img src={ArrowPrev} alt="이전 화살표" />
+          </button>
+        </div>
+        <div>
+          {format(currentMonth, "M")}월 {format(currentMonth, "yyyy")}
+        </div>
+        <div>
+          <button type="button" onClick={nextMonth}>
+            <img src={ArrowNext} alt="다음 화살표" />
+          </button>
+        </div>
+      </Header>
+      <Divider />
+      <DayOfWeek>
+        <div>일</div>
+        <div>월</div>
+        <div>화</div>
+        <div>수</div>
+        <div>목</div>
+        <div>금</div>
+        <div>토</div>
+      </DayOfWeek>
+      <Week onClick={onDateSelect} onKeyDown={onDateSelect} role="presentation">
+        {calendarDays.map((each) => (
+          <Day key={each.id} data-valid={each.isValid} data-value={each.day}>
+            {each.day}
+          </Day>
+        ))}
+      </Week>
+    </CalendarWrapper>
   );
 };
 
@@ -171,88 +164,8 @@ const getCalendarDays = (currentMonth: Date) => {
   }
 };
 
-// function getCalendarDays(currentMonth: Date) {
-//   const monthStartDate = startOfMonth(currentMonth);
-//   const monthEndDate = endOfMonth(monthStartDate);
-
-//   const calendarStartDate = startOfWeek(monthStartDate);
-//   const calendarEndDate = endOfWeek(monthEndDate);
-
-//   const prevMonthDays = getPrevDays();
-//   const currentMonthDays = getCurrentDays();
-//   const nextMonthDays = getNextDays();
-
-//   const calendarDays = prevMonthDays.concat(
-//     currentMonthDays.concat(nextMonthDays)
-//   );
-
-//   return calendarDays;
-
-//   function getPrevDays() {
-//     const prevLastDay = format(endOfMonth(subMonths(currentMonth, 1)), "d");
-//     const monthFirstDay = format(calendarStartDate, "d");
-//     if (format(monthStartDate, "d") === format(calendarStartDate, "d"))
-//       return [];
-//     const prevDays = Array.from(
-//       {
-//         length: parseInt(prevLastDay, 10) - parseInt(monthFirstDay, 10) + 1,
-//       },
-//       (undef, daynumber) => ({
-//         id: crypto.randomUUID(),
-//         day: parseInt(monthFirstDay, 10) + daynumber + 1,
-//         isValid: false,
-//       })
-//     );
-//     return prevDays;
-//   }
-
-//   function getCurrentDays() {
-//     const MonthLastDay = format(monthEndDate, "d");
-//     const currentDays = Array.from(
-//       {
-//         length: parseInt(MonthLastDay, 10),
-//       },
-//       (undef, dayNumber) => ({
-//         id: crypto.randomUUID(),
-//         day: dayNumber + 1,
-//         isValid: true,
-//       })
-//     );
-//     return currentDays;
-//   }
-
-//   function getNextDays() {
-//     const calendarEndDay = format(calendarEndDate, "d");
-//     if (format(monthEndDate, "d") === format(calendarEndDate, "d")) return [];
-//     const nextDays = Array.from(
-//       {
-//         length: parseInt(calendarEndDay, 10),
-//       },
-//       (undef, daynumber) => ({
-//         id: crypto.randomUUID(),
-//         day: daynumber + 1,
-//         isValid: false,
-//       })
-//     );
-//     return nextDays;
-//   }
-// }
-
-const Container = styled.div`
-  position: relative;
-  width: 318px;
-  border-radius: 0;
-  color: ${theme.colors.hover};
-`;
-
-const Title = styled.div`
-  font-weight: bold;
-  font-size: 14px;
-  margin-bottom: 10px;
-  color: #333;
-`;
-
 const CalendarWrapper = styled.div`
+  width: 318px;
   height: fit-content;
   border: 1px solid ${theme.colors.main};
   padding: 30px;
