deb0a8e57dfea28d32913e903f4184cafbea47f1
gandy818 <gandy818@gmail.com>
2023-01-18 23:30:12 +0900

    Feat: 전시 상세 페이지 api 통신 테스트 (현재 주석처리)

--- src/pages/Exhibition.tsx
+++ src/pages/Exhibition.tsx
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
+// import axios, { AxiosResponse } from "axios";
 import styled from "styled-components";
 import MainCard from "../components/exhibition/MainCard";
 import ContentCard from "../components/exhibition/ContentCard";
 import ToggleSwitch from "../components/exhibition/ToggleSwitch";
+// import exhibitionApi, { ExhibitionRes } from "../apis/exhibition";
 
 const Exhibition = () => {
+  // const test = async () => {
+  //   try {
+  //     const res: AxiosResponse<ExhibitionRes> = await exhibitionApi(1);
+  //     const { data } = res;
+  //     console.log(data.title);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
   const [state, setState] = useState("info");
   return (
     <ExhibitionContainer>
+      {/* <button type="button" onClick={test}>
+        api 통신 테스트 버튼
+      </button> */}
       <MainCard
         title="DDP 오픈큐레이팅 vol.26 <Beyond The City: Cultural Monuments>"
         date="2022-12-14 ~ 2023-01-15"
