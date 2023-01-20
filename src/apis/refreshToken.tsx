7b4b5a35f7bf259aa41b0c023eb2f6b7c0d176d0
YesunPark <lynn08082@gmail.com>
2023-01-20 16:15:02 +0900

    Fix: refresh_token 요청 body 토큰에 Bearer 추가 #61

--- src/apis/refreshToken.tsx
+++ src/apis/refreshToken.tsx
@@ -8,33 +8,30 @@ const useRefreshTokenApi = () => {
   const navigate = useNavigate();
   const logOut = useLogOut("refresh_token 만료");
   const token = localStorage.getItem("refresh_token");
-  const reqBody = {
-    refresh_token: token,
-  };
 
   const refreshTokenApi = async () => {
-    try {
-      const res: AxiosResponse<RefreshTokenRes> =
-        //    await apiInstance.post(
-        //   "/refresh",
-        //   reqBody
-        // );
+    if (token)
+      try {
+        const res: AxiosResponse<RefreshTokenRes> = await apiInstance.post(
+          "/refresh",
+          { refresh_token: `Bearer ${token}` }
+        );
         await axios.get("/data/refreshToken.json"); // 테스트용 목데이터
-      const { access_token, refresh_token, errorMsg } = res.data;
-      if (access_token && refresh_token) {
-        localStorage.setItem("access_token", access_token);
-        localStorage.setItem("refresh_token", refresh_token);
+        const { access_token, refresh_token, errorMsg } = res.data;
+        if (access_token && refresh_token) {
+          localStorage.setItem("access_token", access_token);
+          localStorage.setItem("refresh_token", refresh_token);
+        }
+        if (errorMsg === "Forbidden") {
+          await logOut();
+          alert("자동 로그인 기간이 만료되었습니다. 다시 로그인해주세요.");
+          navigate("/login");
+        }
+      } catch (err) {
+        alert(
+          "죄송합니다. 통신오류로 인해 요청을 실패하였습니다.\n다시 시도해주세요."
+        );
       }
-      if (errorMsg === "Forbidden") {
-        await logOut();
-        alert("자동 로그인 기간이 만료되었습니다. 다시 로그인해주세요.");
-        navigate("/login");
-      }
-    } catch (err) {
-      alert(
-        "죄송합니다. 통신오류로 인해 요청을 실패하였습니다.\n다시 시도해주세요."
-      );
-    }
   };
   return refreshTokenApi;
 };
