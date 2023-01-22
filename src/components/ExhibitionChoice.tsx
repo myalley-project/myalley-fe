6a61550b83d30d6f7207cf7eb8ddab315711a233
pglibrary80 <106072839+caffesale@users.noreply.github.com>
2023-01-22 16:23:35 +0900

    Feat: 모달 컴포넌트 완성, Button 컴포넌트 수정 (#103)
    !!! SimpleDialog 컴포넌트 fix후 다시 pr 날리겠습니다.
    
    * Refactor: 버튼 컴포넌트 수정사항 반영
    
    * Docs: ExhibitionChoice 위치 이동
    
    * Update SimpleDialog.tsx

---
+++ src/components/ExhibitionChoice.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const ExhibitionChoice = () => <Container>ExhibitionChoice</Container>;
+
+export default ExhibitionChoice;
+
+const Container = styled.div`
+  width: 1507px;
+  padding: 30px;
+  margin-inline: auto;
+`;
