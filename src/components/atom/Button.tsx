064293ae9569faaee2ec75822fb36ae793d08766
caffesale <pglibrary80@gmail.com>
2023-01-22 13:29:43 +0900

    Refactor: 버튼 컴포넌트 수정사항 반영
    size, variant 프로퍼티만으로 스타일 지정되도록 수정

--- src/components/atom/Button.tsx
+++ src/components/atom/Button.tsx
@@ -1,21 +1,55 @@
 import styled from "styled-components";
 import { theme } from "../../styles/theme";
 
+const backgroundColor = {
+  primary: `${theme.colors.primry70}`,
+  secondary: `${theme.colors.greys60}`,
+  text: `${theme.colors.white100}`,
+};
+
+const hoverBackgroundColor = {
+  primary: `${theme.colors.primry80}`,
+  secondary: `${theme.colors.greys90}`,
+  text: `${theme.colors.greys10}`,
+};
+
+const fontColor = {
+  primary: `${theme.colors.white100}`,
+  secondary: `${theme.colors.white100}`,
+  text: `${theme.colors.greys60}`,
+};
+
+const hoverFontColor = {
+  primary: `${theme.colors.white100}`,
+  secondary: `${theme.colors.white100}`,
+  text: `${theme.colors.greys100}`,
+};
+
+const buttonsize = {
+  small: "20px",
+  large: "40px",
+};
+
 interface ButtonProps {
-  primary: boolean;
+  readonly variant: "primary" | "secondary" | "text";
+  readonly size: "small" | "large";
 }
 
 const Button = styled.button<ButtonProps>`
   width: 175px;
-  padding: 10px 40px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-inline: ${(props) => buttonsize[props.size]};
   border: 0;
   border-radius: 30px;
   font-weight: bold;
   font-size: 1rem;
-  color: ${(props) =>
-    props.primary ? `${theme.colors.white100}` : `${theme.colors.greys60}`};
-  background-color: ${(props) =>
-    props.primary ? `${theme.colors.primry70}` : `${theme.colors.white100}`};
+  color: ${(props) => fontColor[props.variant]};
+  background-color: ${(props) => backgroundColor[props.variant]};
+  &:is(:hover, :focus) {
+    color: ${(props) => hoverFontColor[props.variant]};
+    background-color: ${(props) => hoverBackgroundColor[props.variant]};
+  }
 `;
 
 export default Button;
