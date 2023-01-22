d5c9578c55e7e6874094ad46e17a902e9fea12fb
YesunPark <lynn08082@gmail.com>
2023-01-22 16:03:52 +0900

    Feat: 목데이터 연결해서 메이트 상세페이지 구현 #81

--- src/pages/Mate.tsx
+++ src/pages/Mate.tsx
@@ -4,53 +4,22 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Calender from "../components/Calendar";
 import Comment from "../components/mate/Comment";
+import { MateRes } from "../types/mate";
 import { theme } from "../styles/theme";
 
-// 메이트 모집글 상세페이지_박예선_23.01.19
+// 구현해야 할 부분
+// 댓글 엔터 막기(또는 엔터치면 등록 모달 뜨도록 하기)
+
+// 메이트 모집글 상세페이지_박예선_23.01.22
 const Mate = () => {
   const navigate = useNavigate();
-  const [mateInfo, setMateInfo] = useState({
-    id: 1,
-    title: "모집글 제목",
-    status: "모집 중",
-    mateGender: "여성",
-    mateAge: "연령 무관",
-    availableDate: "2023-02-10",
-    content:
-      "메이트 모집글 내용ㄴㅇㄹㄴㅇㄹㄴㅇㄹ닌아러니ㅏㄹ\nsdlfkjsdlfjsdlf\n",
-    contact: "오픈채팅방 링크",
-    viewCount: 1,
-    createdAt: "2023-01-17",
-    bookmarkCount: 20,
-    bookmarked: false,
-    exhibition: {
-      exhibitionId: 1,
-      exhibitionTitle: "제목2",
-      exhibitionSpace: "국립현대미술관",
-      posterUrl:
-        "https://my-alley-exhibition.s3.ap-northeast-2.amazonaws.com/poster/b769b21c-c858-416a-905c-d5829e35ef34.jpg",
-      exhibitionDuration: "2023-02-01 ~ 2023-05-28",
-      status: "현재 전시",
-    },
-    member: {
-      memberId: 1,
-      memberNickname: "작성자 닉네임",
-      memberProfileImg: "작성자 프로필 이미지",
-      memberGender: "W",
-      memberAge: "1999",
-    },
-  });
-  const mateId = 8;
-
-  interface MateRes {
-    id: number;
-  }
+  const [mateInfo, setMateInfo] = useState<MateRes | null>(null);
+  const [commentTextArea, setCommentTextArea] = useState("");
 
   const getMate = useCallback(async () => {
     try {
       const res: AxiosResponse<MateRes> = await axios.get("/data/mate.json"); // 테스트용 목데이터
-      const { id } = res.data;
-      console.log(id);
+      setMateInfo(res.data);
     } catch (err) {
       alert(
         "죄송합니다.\n전시목록을 불러오는데에 실패하였습니다. 다시 시도해주십시오."
@@ -62,99 +31,144 @@ const Mate = () => {
     getMate();
   }, [getMate]);
 
+  const handleCommentTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.target;
+    setCommentTextArea(value);
+  };
+
   return (
-    <MateContainer>
-      <div className="top-buttons-container">
-        <BtnTransparent>목록</BtnTransparent>
-        <BtnTransparent>이전 글</BtnTransparent>
-        <BtnTransparent>다음 글</BtnTransparent>
-        <BtnTransparent>수정</BtnTransparent>
-        <BtnTransparent>삭제</BtnTransparent>
-      </div>
-      <ExhbCardContainer onClick={() => navigate(`/exhibition/${mateId}`)}>
-        <Thumbnail
-          className="thumbnail"
-          src="https://cdn.pixabay.com/photo/2020/12/23/21/21/macarons-5856039_1280.jpg"
-          alt="thumbnail"
-        />
-        <InfoContainer>
-          <div className="title bold">
-            동대문 디디피 공연보러오세요 눈누냔냐
-          </div>
-          <DetailContainer>
-            <div className="date flex">
-              <div className="detail-name">일정</div>
-              <div>2099-09-09</div>
-            </div>
-            <div className="flex">
-              <div className="detail-name">장소</div>
-              <div>ddp</div>
-            </div>
-          </DetailContainer>
-        </InfoContainer>
-      </ExhbCardContainer>
-      <MateContentContainer>
-        <div className="flex">
-          <div className="title-container">
-            <BtnColored>모집 중</BtnColored>
-            <div className="title">모집글 제목</div>
-            <div className="title-info flex">
-              <div>2023-03-09 18:30</div>
-              <div className="border" />
-              <div>조회수</div>
-              <div className="colored">00</div>
-              <div className="border" />
-              <div>댓글</div>
-              <div className="colored">2</div>
-            </div>
-          </div>
-        </div>
-        <div className="flex">
-          원하는 메이트
+    mateInfo && (
+      <MateContainer>
+        <div className="top-buttons-container flex space-between">
+          <BtnTransparent>목록</BtnTransparent>
+          {/* <BtnTransparent>이전 글</BtnTransparent>
+          <BtnTransparent>다음 글</BtnTransparent> */}
+          {/* 일단 구현 중지 */}
           <div>
-            <div>성별</div>
-            <div>상관없음</div>
-          </div>
-          <div>
-            <div>나이</div>
-            <div>20대 초반 ~ 30대 중반</div>
+            <BtnTransparent>수정</BtnTransparent>
+            <BtnTransparent>삭제</BtnTransparent>
           </div>
         </div>
-        <div>
-          관람예정일
-          <Calender />
-        </div>
-        <div>
-          <span>메이트 설명글</span>
-          <div>니아러닝라ㅓ닝러ㅣㄴ아러ㅣㄴ아러니아러ㅣㄴㅇ러닝런아ㅣ</div>
-        </div>
-        <MemberInfo className="flex">
-          <MemberProfileImg
-            alt="member profile img"
+        <ExhbCardContainer
+          onClick={() =>
+            navigate(`/exhibition/${mateInfo.exhibition.exhibitionId}`)
+          }
+        >
+          <Thumbnail
+            className="thumbnail"
             src="https://cdn.pixabay.com/photo/2020/12/23/21/21/macarons-5856039_1280.jpg"
+            alt="thumbnail"
           />
-          <div>
-            <span>닉네임</span>
-            <div>
-              <span>여자</span>
-              <span>20대 초반</span>
+          <InfoContainer>
+            <div className="title bold">
+              {mateInfo.exhibition.exhibitionTitle}
+            </div>
+            <DetailContainer>
+              <div className="date flex">
+                <div className="detail-name">일정</div>
+                <div>{mateInfo.exhibition.exhibitionDuration}</div>
+              </div>
+              <div className="flex">
+                <div className="detail-name">장소</div>
+                <div>{mateInfo.exhibition.exhibitionSpace}</div>
+              </div>
+            </DetailContainer>
+          </InfoContainer>
+        </ExhbCardContainer>
+        <MateContentContainer>
+          <div className="flex">
+            <div className="title-container">
+              <BtnColored>{mateInfo.status}</BtnColored>
+              <div className="title">{mateInfo.title}</div>
+              <div className="title-info flex">
+                <div>2023-03-09 18:30</div>
+                <div className="border" />
+                <div>조회수</div>
+                <div className="colored">{mateInfo.viewCount}</div>
+                <div className="border" />
+                <div>댓글</div>
+                <div className="colored">00</div>
+              </div>
             </div>
           </div>
-        </MemberInfo>
-        <div>
-          연락가능 메신저 <div>니아러니아러ㅣ나ㅓ </div>
-        </div>
-        <button type="button">저장하기 30</button>
-      </MateContentContainer>
-      <CommentList>
-        <div className="comment-count bold">
-          댓글 <span>9</span>
-        </div>
-        {/* <Comment type="comment" />
+          <SubTitle type="greys90" marginTop={50}>
+            원하는 메이트
+          </SubTitle>
+          <div className="flex">
+            <BorderedDiv width="91">
+              <SubTitle type="greys60" marginTop={0}>
+                성별
+              </SubTitle>
+              <div>{mateInfo.mateGender}</div>
+            </BorderedDiv>
+            <BorderedDiv width="169">
+              <SubTitle type="greys60" marginTop={0}>
+                나이
+              </SubTitle>
+              <div>{mateInfo.mateAge}</div>
+            </BorderedDiv>
+          </div>
+          <div>
+            <SubTitle type="greys90" marginTop={50}>
+              관람예정일
+            </SubTitle>
+            <Calender />
+          </div>
+          <div>
+            <SubTitle type="greys90" marginTop={50}>
+              메이트 설명글
+            </SubTitle>
+            <TextArea type="readOnly" defaultValue={mateInfo.content} />
+          </div>
+          <MemberInfo className="flex">
+            <MemberProfileImg
+              alt="member profile img"
+              src={mateInfo.member.memberProfileImg}
+            />
+            <div>
+              <span>{mateInfo.member.memberNickname}</span>
+              <div>
+                <span>
+                  {mateInfo.member.memberGender === "M" ? "남성" : "여성"}
+                </span>
+                <span>{mateInfo.member.memberAge}</span>
+                {/* n0대 *반으로 변경해야함 */}
+              </div>
+            </div>
+          </MemberInfo>
+          <div>
+            <SubTitle type="greys90" marginTop={50}>
+              연락가능 메신저
+            </SubTitle>
+            <div>{mateInfo.contact}</div>
+          </div>
+          <button type="button">저장하기 {mateInfo.bookmarkCount}</button>
+        </MateContentContainer>
+        <CommentList>
+          <div className="comment-count bold">
+            댓글 <span>0</span>
+          </div>
+          {/* <Comment type="comment" />
         <Comment type="reply" />
         댓글기능 추가되면 추가하기 */}
-      </CommentList>
-    </MateContainer>
+          <SubTitle type="greys90" marginTop={30}>
+            현재보고있는 회원 닉네임
+          </SubTitle>
+          <TextAreaContainer>
+            <TextArea
+              type="textArea"
+              placeholder="내용을 입력해주세요."
+              value={commentTextArea}
+              onChange={handleCommentTextArea}
+              maxLength={150}
+            />
+            <div className="input-status">
+              <span>{commentTextArea.length}</span>/<span>150</span>
+            </div>
+          </TextAreaContainer>
+        </CommentList>
+      </MateContainer>
+    )
   );
 };
 
@@ -169,6 +183,9 @@ const MateContainer = styled.div`
   }
   .flex {
     display: flex;
+  }
+  .space-between {
+    justify-content: space-between;
   }
   .bold {
     font-weight: 700;
@@ -226,7 +243,6 @@ const MateContentContainer = styled.div`
   border: 1px solid ${theme.colors.greys40};
   .title-container {
     width: 100%;
-    margin-bottom: 50px;
     padding-bottom: 15px;
     border-bottom: 1px solid ${theme.colors.greys40};
     border-radius: 0;
@@ -255,7 +271,33 @@ const MateContentContainer = styled.div`
   }
 `;
 
-const MemberInfo = styled.div``;
+const SubTitle = styled.div<{ type: "greys60" | "greys90"; marginTop: number }>`
+  margin-top: ${(props) => `${props.marginTop}px`};
+  margin-bottom: ${(props) => (props.type === "greys60" ? "0" : "10px")};
+  color: ${(props) =>
+    props.type === "greys60" ? theme.colors.greys60 : theme.colors.greys90};
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 20px;
+`;
+
+const BorderedDiv = styled.div<{ width: string }>`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: ${(props) => props.width}+ "px";
+  height: 90px;
+  margin-right: 10px;
+  padding: 20px;
+  border: 1px solid ${theme.colors.greys40};
+  color: ${theme.colors.greys90};
+  font-size: 14px;
+  font-weight: 700;
+`;
+
+const MemberInfo = styled.div`
+  margin-top: 30px;
+`;
 
 const MemberProfileImg = styled.img`
   width: 78px;
@@ -278,6 +320,33 @@ const CommentList = styled.div`
   }
 `;
 
+const TextArea = styled.textarea<{ type: "textArea" | "readOnly" }>`
+  width: 100%;
+  height: 150px;
+  padding: ${(props) => (props.type === "textArea" ? "20px" : "0")};
+  border: ${(props) =>
+    props.type === "textArea" ? `1px solid ${theme.colors.greys40}` : "none"};
+  border-radius: ${(props) => (props.type === "textArea" ? "30px" : "0")};
+  color: ${(props) =>
+    props.type === "textArea" ? theme.colors.greys60 : theme.colors.greys90};
+  font-size: 14px;
+  font-weight: 500;
+  resize: none;
+`;
+
+const TextAreaContainer = styled.div`
+  position: relative;
+  width: 100%;
+  .input-status {
+    position: absolute;
+    right: 20px;
+    bottom: 20px;
+    color: ${theme.colors.greys60};
+    font-size: 14px;
+    font-weight: 500;
+  }
+`;
+
 const BtnColored = styled.button`
   height: 36px;
   padding: 0 20px;
@@ -290,4 +359,7 @@ const BtnTransparent = styled.button`
   height: 40px;
   padding: 0 20px;
   border: 1px solid ${theme.colors.greys40};
+  &:nth-child(1) {
+    margin-right: 10px;
+  }
 `;
