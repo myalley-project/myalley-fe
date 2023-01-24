97835f08e885561514a272301bf6bedc4ee17289
gandy818 <gandy818@gmail.com>
2023-01-24 19:15:52 +0900

    Fix: mate type 수정

--- src/types/mateList.d.ts
+++ src/types/mateList.d.ts
@@ -2,8 +2,8 @@ export interface Mate {
   mateId: number;
   title: string;
   availableDate: string;
-  status: string;
-  mateGender: string;
+  status: "모집 중" | "모집 완료";
+  mateGender: "남성" | "여성" | "성별무관";
   mateAge: string;
   createdAt: string;
   viewCount: number;
@@ -14,6 +14,6 @@ export interface Mate {
     exhibitionTitle: string;
     exhibitionSpace: string;
     posterUrl: string;
-    exhibitionStatus: string;
+    exhibitionStatus: "지난 전시" | "현재 전시" | "예정 전시";
   };
 }
