a284fa970d3ff50d4647a8494dcfe24c6c41d173
caffesale <pglibrary80@gmail.com>
2023-01-25 19:46:59 +0900

    Feat: 메인 페이지 Link 추가

--- src/components/MainArticle.tsx
+++ src/components/MainArticle.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import ArrowButton from "../assets/icons/arrowRight.svg";
 import { theme } from "../styles/theme";
 
@@ -11,10 +12,10 @@ const MainArticle = () => (
         <h3>전시회</h3>
         <p>다양한 전시 정보를 찾아보세요</p>
       </div>
-      <button type="button">
+      <StyledLink to="/exhibition-list">
         <span>자세히 보기</span>
         <img src={ArrowButton} alt="" />
-      </button>
+      </StyledLink>
     </Article>
     <Article>
       <Icon />
@@ -22,10 +23,10 @@ const MainArticle = () => (
         <h3>전시 리뷰</h3>
         <p>다양한 전시 리뷰 를 찾아보세요</p>
       </div>
-      <button type="button">
+      <StyledLink to="">
         <span>자세히 보기</span>
         <img src={ArrowButton} alt="" />
-      </button>
+      </StyledLink>
     </Article>
     <Article>
       <Icon />
@@ -33,10 +34,10 @@ const MainArticle = () => (
         <h3>메이트</h3>
         <p>전시회에 같이 갈 친구를 찾아보세요</p>
       </div>
-      <button type="button">
+      <StyledLink to="">
         <span>자세히 보기</span>
         <img src={ArrowButton} alt="" />
-      </button>
+      </StyledLink>
     </Article>
   </ArticleContainer>
 );
@@ -75,19 +76,21 @@ const Article = styled.article`
     color: #9c9c9c;
     font-size: 14px;
   }
-  & > button {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 4px;
-    color: ${theme.colors.greys60};
-    font-weight: 700;
-    font-size: 14px;
-  }
 `;
 
 const Icon = styled.div`
   width: 87px;
   height: 87px;
   background-color: #ffcaca;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  color: ${theme.colors.greys60};
+  font-weight: 700;
+  font-size: 14px;
+  text-decoration: none;
 `;
