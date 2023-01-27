02ca04d831674adc51ca6193f378f2cb46ab5ea0
YesunPark <lynn08082@gmail.com>
2023-01-27 16:44:10 +0900

    Fix: 메이트글 type 수정 #93

--- src/types/mateList.d.ts
+++ src/types/mateList.d.ts
@@ -9,8 +9,8 @@ export interface Mate {
   mateAge: string; // n0대 x반 ~ n0대 x반
   createdAt: string; // YYYY-MM-DD
   viewCount: number;
-  memberId: number;
-  memberNickname: string;
+  memberId?: number; // 메이트글 목록 조회 페이지에만 해당
+  memberNickname?: string; // 메이트글 목록 조회 페이지에만 해당
   exhibition: {
     exhibitionId: number;
     exhibitionTitle: string;
