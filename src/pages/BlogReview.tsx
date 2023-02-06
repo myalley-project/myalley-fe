602010427b96e88bfc8d688bef5fec5abc0cbe2e
YesunPark <lynn08082@gmail.com>
2023-02-07 02:10:38 +0900

    Refactor: 사용하지 않는 import 삭제

--- src/pages/BlogReview.tsx
+++ src/pages/BlogReview.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { useQuery } from "react-query";
