0293053be9a15861d8815339442b380465031c1d
YesunPark <lynn08082@gmail.com>
2023-02-07 19:45:09 +0900

    Fix: 토큰만료시에도 로그아웃 버튼 누르면 바로 로그아웃 되도록 수정 #251

--- src/apis/logOut.tsx
+++ src/apis/logOut.tsx
@@ -5,7 +5,7 @@ import apiInstance from "../utils/apiInstance";
 import isApiError, { errorAlert } from "../utils/isApiError";
 import removeLocalStorageItem from "../utils/removeLocalStorageItem";
 
-// 로그아웃 커스텀훅_박예선_2023.02.06
+// 로그아웃 커스텀훅_박예선_2023.02.07
 // 사용법: const logOut = useLogOut();
 const useLogOut = () => {
   const navigate = useNavigate();
@@ -26,24 +26,18 @@ const useLogOut = () => {
     } catch (err) {
       const errorRes = isApiError(err);
       if (errorRes === "accessToken 만료") {
-        console.log("리프레시토큰 메세지 받음");
         try {
           await refreshTokenApi();
-          console.log("토큰 잘 받음");
           const res: AxiosResponse<LogOutRes> = await apiInstance.post(
             "/api/me/logout"
           );
-          console.log("다시 로그아웃 실행");
-          console.log("다시 실행한 로그아웃 응답", res);
           const { resultCode } = res.data;
           if (resultCode === 200) {
-            console.log("다시 로그아웃 성공");
             removeLocalStorageItem();
             alert("로그아웃되었습니다.");
             navigate("/");
           }
         } catch {
-          console.log("로그아웃 아예 실패");
           errorAlert();
         }
       }
