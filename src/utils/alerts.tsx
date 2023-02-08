49733f482b68f2f2391c8de01ed220cf9eb9b9d7
YesunPark <lynn08082@gmail.com>
2023-02-08 13:01:37 +0900

    Refactor: alertError function키워드로 변경

--- src/utils/alerts.tsx
+++ src/utils/alerts.tsx
@@ -4,6 +4,6 @@ export function alertPreparing() {
 }
 
 // 에러 alert실행 함수_박예선_23.01.23
-export const alertError = () => {
+export function alertError() {
   alert("죄송합니다.\n네트워크 오류입니다. 다시 시도해주십시오.");
-};
+}
