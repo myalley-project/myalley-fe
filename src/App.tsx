d130b1d0208aaafb43fd15df6a1689182af34a6d
caffesale <pglibrary80@gmail.com>
2023-02-16 22:29:31 +0900

    Feat: silentrefresh 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -18,37 +18,42 @@ import BlogReviewWrite from "./pages/BlogReviewWrite";
 import BlogReviewUpdate from "./pages/BlogReviewModify";
 import BlogReview from "./pages/BlogReview";
 import Footer from "./components/Footer";
+import useSilentRefresh from "./hooks/useSilentRefresh";
 
-const App = () => (
-  <BrowserRouter>
-    <ScrollTop />
-    <Nav />
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup?" element={<SignUp />} />
-      <Route path="/exhibition-list" element={<ExhibitionList />} />
-      <Route path="/exhibition/:id" element={<Exhibition />} />
-      <Route
-        path="/exhibition/:id/edit"
-        element={<ExhibitionWrite mode="edit" />}
-      />
-      <Route
-        path="/exhibition-write"
-        element={<ExhibitionWrite mode="create" />}
-      />
-      <Route path="/mypage/:subpages" element={<Mypage />} />
-      <Route path="/mate-list" element={<MateList />} />
-      <Route path="/mate/:id" element={<Mate />} />
-      <Route path="/mate-write" element={<MateWrite />} />
-      <Route path="/blogreview-list" element={<BlogReviewList />} />
-      <Route path="/blogreview-write" element={<BlogReviewWrite />} />
-      <Route path="/blogreview-edit" element={<BlogReviewUpdate />} />
-      <Route path="/blog/:id" element={<BlogReview />} />
-    </Routes>
-    <ReactQueryDevtools />
-    <Footer />
-  </BrowserRouter>
-);
+const App = () => {
+  useSilentRefresh();
+
+  return (
+    <BrowserRouter>
+      <ScrollTop />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup?" element={<SignUp />} />
+        <Route path="/exhibition-list" element={<ExhibitionList />} />
+        <Route path="/exhibition/:id" element={<Exhibition />} />
+        <Route
+          path="/exhibition/:id/edit"
+          element={<ExhibitionWrite mode="edit" />}
+        />
+        <Route
+          path="/exhibition-write"
+          element={<ExhibitionWrite mode="create" />}
+        />
+        <Route path="/mypage/:subpages" element={<Mypage />} />
+        <Route path="/mate-list" element={<MateList />} />
+        <Route path="/mate/:id" element={<Mate />} />
+        <Route path="/mate-write" element={<MateWrite />} />
+        <Route path="/blogreview-list" element={<BlogReviewList />} />
+        <Route path="/blogreview-write" element={<BlogReviewWrite />} />
+        <Route path="/blogreview-edit" element={<BlogReviewUpdate />} />
+        <Route path="/blog/:id" element={<BlogReview />} />
+      </Routes>
+      <ReactQueryDevtools />
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/hooks/useSilentRefresh.tsx
+++ src/hooks/useSilentRefresh.tsx
@@ -6,6 +6,14 @@ import apiInstance from "../utils/apiInstance";
 
 const useSilentRefresh = () => {
   const [refreshStop, setRefreshStop] = useState(false);
+  const RefreshExpireDate = JSON.parse(
+    localStorage.getItem("refreshExpireDate") as string
+  ) as string;
+
+  if (!RefreshExpireDate || new Date(RefreshExpireDate) > new Date()) {
+    localStorage.clear();
+    alert("자동 로그인 기간이 만료되었습니다. 다시 로그인해주세요");
+  }
 
   useQuery(["silentRefresh"], getSilentRefreshToken, {
     refetchOnWindowFocus: false,
@@ -16,6 +24,8 @@ const useSilentRefresh = () => {
     refetchIntervalInBackground: true,
     onError: () => {
       setRefreshStop(true);
+      localStorage.clear();
+      alert("로그인 중 문제가 생겼습니다. 다시 로그인해주세요.");
     },
     onSuccess: (data) => {
       const accessToken = data?.accessToken;
