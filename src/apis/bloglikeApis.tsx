1857dc99259a3d74d83a74bd897e5007fd4b1c31
caffesale <pglibrary80@gmail.com>
2023-02-26 09:00:03 +0900

    Fix: 블로그리뷰 좋아요 추가/삭제

--- src/apis/bloglikeApis.tsx
+++ src/apis/bloglikeApis.tsx
@@ -2,14 +2,6 @@ import axios, { AxiosInstance } from "axios";
 import apiInstance from "../utils/apiInstance";
 
 const bloglikeApis = {
-  like: async (blogId: number) => {
-    const response = await apiInstance.post(`api/likes/blogs/${blogId}`);
-    return response;
-  },
-  dislike: async (blogId: number) => {
-    const response = await apiInstance.delete(`api/likes/blogs/${blogId}`);
-    return response;
-  },
   putlike: async (blogId: number) => {
     const response = await apiInstance.put(`/api/blogs/likes/${blogId}`);
     return response;
