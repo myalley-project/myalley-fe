2ff6b3c2a3ed66cdb551e1e141b603c905746a88
caffesale <pglibrary80@gmail.com>
2023-02-27 23:18:47 +0900

    Docs: NotFound 메세지 받도록 수정

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const NotFound = () => {
+const NotFound = (message: string) => {
   const navigate = useNavigate();
   React.useEffect(() => {
-    alert("데이터를 불러올 수 없습니다.");
+    alert(message);
     navigate("/");
   });
 
