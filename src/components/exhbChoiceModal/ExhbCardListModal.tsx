e6305524571b309a569ba373f7537cc314892272
pglibrary80 <106072839+caffesale@users.noreply.github.com>
2023-03-04 12:23:38 +0900

    Feat: 전시회 모달 스크롤화 (#451)
    * Feat: 전시회 선택 모달 스크롤, 페이지네이션 적용
    
    * Design: 블로그 리뷰 작성시 plus 아이콘 크기 수정
    
    * Update ExhibitionSelect.tsx
    
    * Docs: 컨벤션 수정
    
    * Docs: 불필요한 import 삭제
    
    mouse event 삭제

--- src/components/exhbChoiceModal/ExhbCardListModal.tsx
+++ src/components/exhbChoiceModal/ExhbCardListModal.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import { useNavigate } from "react-router-dom";
-import styled, { ThemeConsumer } from "styled-components";
+import styled from "styled-components";
+import Pagination from "../Pagination";
 import { Exhibition } from "../../types/exhbList";
 import { theme } from "../../styles/theme";
 import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/scrollbar";
 
 interface ExhbCardListType {
-  exhbList: Exhibition[]; // 전시회 목록 데이터
+  exhbList: Exhibition[];
   type: "myPage" | "exhbList";
   getExhbInfo: (
     url: string,
@@ -17,12 +19,26 @@ interface ExhbCardListType {
     status: string
   ) => void;
   handleModal: () => void;
+  pages: {
+    started: number;
+    selected: number;
+  };
+  setPages: React.Dispatch<
+    React.SetStateAction<{
+      started: number;
+      selected: number;
+    }>
+  >;
+  totalPage: number;
 }
 const ExhbCardListModal = ({
   exhbList,
   type,
   getExhbInfo,
   handleModal,
+  pages,
+  setPages,
+  totalPage,
 }: ExhbCardListType) => {
   const navigate = useNavigate();
 
@@ -41,48 +57,63 @@ const ExhbCardListModal = ({
   };
 
   return (
-    <div>
-      <Swiper slidesPerView={4} spaceBetween={30} className="mySwiper">
-        {exhbList.map((exhb) => {
-          const { id, title, space, duration, posterUrl, viewCount, status } =
-            exhb;
-          return (
-            <SwiperSlide key={exhb.id}>
-              <ExhibitionCard
-                onClick={() => {
-                  HandleClick(posterUrl, id, title, duration, status);
-                  handleModal();
-                }}
-              >
-                <Thumbnail>
-                  <img alt="thumbnail" src={posterUrl} />
-                </Thumbnail>
-                <ContentBox>
-                  <ContentTop>
-                    <h2>{title}</h2>
-                    <p>{space}</p>
-                  </ContentTop>
-                  <ContentFooter>
-                    <div className="exhb-period">{duration}</div>
-                    <span className="viewCount">조회수 {viewCount}</span>
-                  </ContentFooter>
-                </ContentBox>
-              </ExhibitionCard>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </div>
+    <Container>
+      {exhbList.map((exhb) => {
+        const { id, title, space, duration, posterUrl, viewCount, status } =
+          exhb;
+        return (
+          <ExhibitionCard
+            key={exhb.id}
+            onClick={() => {
+              HandleClick(posterUrl, id, title, duration, status);
+              handleModal();
+            }}
+          >
+            <Thumbnail>
+              <img alt="thumbnail" src={posterUrl} />
+            </Thumbnail>
+            <ContentBox>
+              <ContentTop>
+                <h2>{title}</h2>
+                <p>{space}</p>
+              </ContentTop>
+              <ContentFooter>
+                <div className="exhb-period">{duration}</div>
+                <span className="viewCount">조회수 {viewCount}</span>
+              </ContentFooter>
+            </ContentBox>
+          </ExhibitionCard>
+        );
+      })}
+      <PageContainer>
+        <Pagination pages={pages} setPages={setPages} totalPage={totalPage} />
+      </PageContainer>
+    </Container>
   );
 };
 
 export default ExhbCardListModal;
 
+const Container = styled.div`
+  display: grid;
+  position: relative;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 2rem;
+  max-height: calc(100vh - 400px);
+  overflow-y: auto;
+  @media screen and (max-width: 1200px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media screen and (max-width: 1024px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media screen and (max-width: 720px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
 const ExhibitionCard = styled.div`
-  width: 338px;
-  height: fit-content;
   border: 2px solid ${theme.colors.greys40};
-  /* border-radius: 30px; */
   cursor: pointer;
   :hover {
     border: 2px solid ${theme.colors.primry60};
@@ -112,9 +143,7 @@ const ContentTop = styled.div`
     font-weight: bold;
     font-size: 20px;
     margin-bottom: 6px;
-    white-space: nowrap;
     line-height: 21px;
-    overflow: clip;
   }
   & > p {
     color: ${theme.colors.greys60};
@@ -132,4 +161,8 @@ const ContentFooter = styled.div`
   .viewCount {
     color: ${theme.colors.greys60};
   }
+`;
+
+const PageContainer = styled.div`
+  grid-column: 1/-1;
 `;
